13a963d0a129ed1dc1bed0e3f4d812a3d1e16b8f
Denis Krestov <rupo@list.ru>
2024-11-17 13:47:38 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,362 @@
+const data = [
+    {
+        name: "Финансовый аналитик",
+        mainSkills: ["Excel", "SQL", "VBA", "1С"],
+        otherSkills: ["Power BI", "Python"],
+    },
+    {
+        name: "Предприниматель",
+        mainSkills: ["1C", "Excel", "Power BI"],
+        otherSkills: [
+            "Google Analytics",
+            "Яндекс.Метрика",
+            "Python",
+            "SQL",
+            "Tilda",
+        ],
+    },
+    {
+        name: "Продуктовый дизайнер",
+        mainSkills: [
+            "Figma",
+            "Sketch",
+            "Illustrator",
+            "Photoshop",
+            "Principle",
+            "Tilda",
+        ],
+        otherSkills: ["Shopify", "Protopie", "Cinema 4D"],
+    },
+    {
+        name: "Менеджер проекта",
+        mainSkills: [
+            "Visio",
+            "1C",
+            "Google Analytics",
+            "Яндекс.Метрика",
+            "Python",
+            "SQL",
+            "Tilda",
+        ],
+        otherSkills: ["Figma", "Sketch", "Shopify"],
+    },
+    {
+        name: "Финансовый менеджер",
+        mainSkills: ["1C", "Excel", "Power BI"],
+        otherSkills: ["BPMN"],
+    },
+    {
+        name: "Руководитель финансового департамента компании",
+        mainSkills: ["Sketch", "Figma"],
+        otherSkills: ["Shopify", "HQL"],
+    },
+
+    {
+        name: "Продуктовый аналитик",
+        mainSkills: [
+            "Google Analytics",
+            "Яндекс.Метрика",
+            "SQL",
+            "Power BI",
+            "Python",
+            "Excel",
+        ],
+        otherSkills: ["HQL", "Tableau", "R", "Machine learning"],
+    },
+
+    {
+        name: "Руководитель финансового продукта",
+        mainSkills: ["Visio"],
+        otherSkills: ["Python"],
+    },
+    {
+        name: "Менеджер по маркетингу",
+        mainSkills: [
+            "Google Analytics",
+            "Яндекс.Метрика",
+            "Google Ads",
+            "Ahrefs",
+            "Главред",
+            "My Target",
+        ],
+        otherSkills: ["Tilda", "Photoshop", "Xenu", "Python"],
+    },
+
+    {
+        name: "Менеджер по цифровой трансформации",
+        mainSkills: [
+            "Visio",
+            "Google Analytics",
+            "Яндекс.Метрика",
+            "Python",
+            "SQL",
+            "Tilda",
+        ],
+        otherSkills: ["Figma", "Sketch", "Shopify"],
+    },
+]
+const rolesArr = data.reduce((acc, item) => {
+    return acc.concat(item.name);
+}, [])
+
+const skillsArr = Array.from(new Set(data.reduce((acc, item) => {
+    return acc.concat(item.mainSkills, item.otherSkills);
+}, [])))
+
+
+const COLOR_GREY = 'rgba(173, 173, 173, 1)'
+const COLOR_ORANGE_WEAK = 'rgba(255, 212, 173, 1)'
+const COLOR_ORANGE_STRONG = 'rgba(255, 122, 0, 1)'
+const COLOR_GREEN = 'rgba(0, 163, 114, 1)'
+const COLOR_VIOLET = 'rgba(143, 89, 185, 1)'
+
+const numberOfRoles = 10;
+const numberOfSkills = 28;
+const rolesAngleStep = (2 * Math.PI) / numberOfRoles;
+const skillsAngleStep = (2 * Math.PI) / numberOfSkills;
+const innerRingRadius = 254 / 2;
+const outerRingRadius = 532 / 2;
+
+
+//----canvas properties
+const canvas = document.getElementById('canvas_plot');
+const c = canvas.getContext('2d');
+
+const canvasWidth = canvas.clientWidth;
+const canvasHeight = canvas.clientHeight;
+
+const canvasCenter = {
+    x: canvasWidth / 2,
+    y: canvasHeight / 2,
+}
+//----
+
+
+let selectedRoleIndex = null;
+let selectedSkillIndex = null;
+let selectedRole_xy = null
+const roles_xy = []
+const skills_xy = []
+
+function drawRing(centerX, centerY, radius) {
+    c.lineWidth = 3;
+    c.strokeStyle = COLOR_GREY;
+    c.beginPath();
+    c.arc(centerX, centerY, radius, 0, Math.PI * 2);
+    c.stroke();
+    c.closePath();
+}
+
+function drawPoint(x, y, type) {
+    let fillColor
+    let isActive = false
+    if (type.startsWith('active')) {
+        isActive = true
+    }
+
+    switch (type) {
+        case 'role' :
+            fillColor = COLOR_GREY
+            break;
+        case 'skill' :
+            fillColor = COLOR_ORANGE_WEAK
+            break;
+        case 'activeRole' :
+            fillColor = COLOR_GREEN
+            break;
+        case 'activeSkill' :
+            fillColor = COLOR_ORANGE_STRONG
+            break;
+    }
+
+    c.beginPath();
+
+    if (isActive) {
+        c.beginPath()
+        c.fillStyle = fillColor
+        c.arc(x, y, 18, 0, Math.PI * 2, false);
+        c.fill();
+        c.closePath()
+        c.beginPath()
+        c.fillStyle = 'white';
+        c.arc(x, y, 16, 0, Math.PI * 2, false);
+        c.fill();
+        c.closePath()
+    }
+
+    c.beginPath();
+    c.fillStyle = fillColor
+    c.arc(x, y, 12, 0, Math.PI * 2, false);
+    c.fill();
+    c.closePath()
+
+    c.closePath();
+}
+
+function drawText(text, textX, textY) {
+    const words = text.split(' ');
+    let line = '';
+    const lineHeight = 12;
+    const maxWidth = 75;
+
+    for (let j = 0; j < words.length; j++) {
+        const testLine = line + words[j] + ' ';
+        const testWidth = c.measureText(testLine).width;
+
+        if (testWidth > maxWidth && j > 0) {
+            c.textAlign = 'center'
+            c.fillStyle = 'black'
+            c.fillText(line, textX, textY);
+            line = words[j] + ' ';
+            textY += lineHeight;
+        } else {
+            line = testLine;
+        }
+    }
+    c.textAlign = 'center'
+    c.fillStyle = 'black'
+    c.fillText(line, textX, textY);
+}
+
+function draw() {
+    roles_xy.length = 0;
+    skills_xy.length = 0;
+    c.clearRect(0, 0, canvasWidth, canvasHeight);
+    // Draw rings
+    drawRing(canvasCenter.x, canvasCenter.y, innerRingRadius);
+    drawRing(canvasCenter.x, canvasCenter.y, outerRingRadius);
+
+    // Draw roles
+    for (let i = 0; i < numberOfRoles; i++) {
+        const angle = i * rolesAngleStep;
+        const roleX = canvasCenter.x + innerRingRadius * Math.cos(angle);
+        const roleY = canvasCenter.y + innerRingRadius * Math.sin(angle);
+        roles_xy.push([roleX, roleY, rolesArr[i]]);
+        const type = (selectedRoleIndex === i) ? 'activeRole' : 'role';
+        drawPoint(roleX, roleY, type);
+
+
+        // Calculate position for role text
+        let textX = canvasCenter.x + (innerRingRadius + 60) * Math.cos(angle); // 30 pixels away from inner ring
+        let textY = canvasCenter.y + (innerRingRadius + 60) * Math.sin(angle); // 30 pixels away from inner ring
+
+        drawText(rolesArr[i], textX, textY);
+    }
+
+    // Draw skills
+    for (let i = 0; i < numberOfSkills; i++) {
+        const angle = i * skillsAngleStep;
+        const skillX = canvasCenter.x + outerRingRadius * Math.cos(angle);
+        const skillY = canvasCenter.y + outerRingRadius * Math.sin(angle);
+        skills_xy.push([skillX, skillY, skillsArr[i]]);
+        const type = (selectedSkillIndex === i) ? 'activeSkill' : 'skill';
+        drawPoint(skillX, skillY, type);
+
+        // Calculate position for role text
+        let textX = canvasCenter.x + (outerRingRadius + 40) * Math.cos(angle); // 30 pixels away from inner ring
+        let textY = canvasCenter.y + (outerRingRadius + 40) * Math.sin(angle); // 30 pixels away from inner ring
+        drawText(skillsArr[i], textX, textY);
+
+    }
+
+    //Draw lines
+    let otherSkillsForRole_xy, mainSkillsForRole_xy
+    if (selectedRole_xy !== null) {
+        const skillsForRole = data.find(item => item.name === selectedRole_xy[2])
+        otherSkillsForRole_xy = skills_xy.filter(item => skillsForRole.otherSkills.includes(item[2]));
+        mainSkillsForRole_xy = skills_xy.filter(item => skillsForRole.mainSkills.includes(item[2]));
+        console.log(otherSkillsForRole_xy)
+    }
+
+
+    if (selectedRoleIndex !== null) {
+        for (let i = 0; i < otherSkillsForRole_xy.length; i++) {
+            c.beginPath();
+            c.strokeStyle = COLOR_ORANGE_STRONG
+            c.moveTo(...selectedRole_xy)
+            c.lineTo(otherSkillsForRole_xy[i][0], otherSkillsForRole_xy[i][1])
+            c.stroke()
+            c.closePath()
+        }
+
+        for (let i = 0; i < mainSkillsForRole_xy.length; i++) {
+            c.beginPath();
+            c.strokeStyle = COLOR_VIOLET
+            c.moveTo(...selectedRole_xy)
+            c.lineTo(mainSkillsForRole_xy[i][0], mainSkillsForRole_xy[i][1])
+            c.stroke()
+            c.closePath()
+        }
+    }
+}
+
+//Click handler
+canvas.addEventListener('click', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    for (let i = 0; i < numberOfRoles; i++) {
+        const distance = Math.sqrt((mouseX - roles_xy[i][0]) ** 2 + (mouseY - roles_xy[i][1]) ** 2);
+        if (distance < 12) {
+            selectedRoleIndex = i;
+            selectedRole_xy = [roles_xy[i][0], roles_xy[i][1], roles_xy[i][2]];
+            draw();
+            return;
+        }
+    }
+    for (let i = 0; i < numberOfSkills; i++) {
+        const distance = Math.sqrt((mouseX - skills_xy[i][0]) ** 2 + (mouseY - skills_xy[i][1]) ** 2);
+        if (distance < 12) {
+            selectedSkillIndex = i;
+            draw();
+            return;
+        }
+    }
+});
+
+canvas.addEventListener('mousemove', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+    let isOverPoint = false;
+
+    for (let i = 0; i < numberOfRoles; i++) {
+        const distance = Math.sqrt((mouseX - roles_xy[i][0]) ** 2 + (mouseY - roles_xy[i][1]) ** 2);
+        if (distance < 12) {
+            isOverPoint = true;
+            break;
+        }
+    }
+    for (let i = 0; i < numberOfSkills; i++) {
+        const distance = Math.sqrt((mouseX - skills_xy[i][0]) ** 2 + (mouseY - skills_xy[i][1]) ** 2);
+        if (distance < 12) {
+            isOverPoint = true;
+            break;
+        }
+    }
+
+    // Изменение курсора
+    canvas.style.cursor = isOverPoint ? 'pointer' : 'default';
+});
+
+draw();
+
+
+const var1 = {
+    'Дизайнер': [500, 400],
+    'Менеджер': [150, 300],
+    'Предприниматель': [200, 250],
+}
+
+const var2 = {
+    '500 400': 'Дизайнер',
+    '150 300': 'Менеджер',
+    '200, 250': 'Предприниматель',
+}
+
+const var3 = [
+    ['Дизайнер', 500, 400],
+    ['Менеджер', 150, 300],
+    ['Предприниматель', 200, 250],
+]
